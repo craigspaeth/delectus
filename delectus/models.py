e80fc48f59a7aafa15b946a9b9b7111abaa0ee8d
Craig Spaeth <craigspaeth@gmail.com>
2013-05-20 20:32:36 -0400

    wired in tapes

--- delectus/models.py
+++ delectus/models.py
@@ -14,11 +14,14 @@ class Tape(models.Model):
     title = models.CharField(max_length=200)
     alertnate_title = models.CharField(max_length=200, blank=True)
     date = models.CharField(max_length=200, blank=True)
-    hours = models.IntegerField(blank=True)
-    minutes = models.IntegerField(blank=True)
-    seconds = models.IntegerField(blank=True)
+    hours = models.IntegerField(null=True, blank=True)
+    minutes = models.IntegerField(null=True, blank=True)
+    seconds = models.IntegerField(null=True, blank=True)
     country = models.CharField(max_length=200, blank=True)
     format = models.CharField(max_length=200, blank=True)
     color = models.CharField(max_length=50, blank=True, choices=COLOR_CHOICES)
     audio = models.CharField(max_length=50, blank=True, choices=AUDIO_CHOICES)
     keywords = models.TextField(blank=True)
+    
+    def __unicode__(self):
+    	return self.title
